9d49812482924aad0616a98328660a822ef6c87f
NhendeK <kiyabonhende@gmail.com>
2023-10-31 06:11:02 +0300

    The layout Improve following Blade Template Inheritance. The Layout.layout file was sectioned to have header(Navigation panel) Aside Navigation Content Footer Added urls for Overview in aside navigation and Students in header portion of Apps Kiyabo K. Nhende 31-10-2023, 06:10

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,11 @@ Route::get('/', function(){
 
 
 // Route::get('/students', 'App\Http\Controllers\StudentController@index');
-Route::get('/students', [StudentController::class, 'index'])->name('student.home');
+Route::get('/students', [StudentController::class, 'index'])->name('students.home');
 
 Route::get('/layout', [StudentController::class,'layout'])->name('layout.layout');
+
+// Route::get('/students',[StudentController::class, 'welcome'])->name('students.welcome');
 
 
 
