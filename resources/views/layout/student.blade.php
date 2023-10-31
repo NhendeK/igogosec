d8304d40ff88cf884e12d9f0fd4597cdba6b57d7
NhendeK <kiyabonhende@gmail.com>
2023-10-31 05:38:05 +0300

    Some Improvement on the Layout of the page App Added: Student Controller     Routes for Layout and Students     Images in public foler

---
+++ resources/views/layout/student.blade.php
@@ -0,0 +1,25 @@
+{{-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     @vite('resources/css/app.css')
+    <title>Students</title>
+</head>
+<body>
+    
+<h1 class="text-5xl">Welcome to Studets Web App</h1>
+
+<div class="main">
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Consequuntur voluptate quisquam hic alias? 
+        Officia neque accusamus aliquid,
+         voluptatum recusandae quisquam ea! 
+        Hic ratione cupiditate unde distinctio eos quasi omnis enim.
+        <p>
+</div>
+
+</body>
+</html> --}}
